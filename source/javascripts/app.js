be8e2297412a01d32b67d55bbbf77b7ad3dbb0a0
timsvoice <timsethvoice@gmail.com>
2014-05-06 13:13:41 -0400

    added branding

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,1 +1,12 @@
 $(document).foundation();
+
+//splash 
+function resizesplash() {
+      var heights = window.innerHeight;
+      document.getElementById("splash").style.height = heights + "px";
+  }
+
+  resizesplash();
+  window.onresize = function() {
+      resizesplash();
+};
